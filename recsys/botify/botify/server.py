90c8bcaac03f234dce22d1c25f65308d31b844d1
Nikolay Anokhin <n.anokhin@corp.mail.ru>
2021-09-08 18:42:04 +0300

    Run simulator over several 'days'

--- recsys/botify/botify/server.py
+++ recsys/botify/botify/server.py
@@ -54,4 +54,4 @@ api.add_resource(NextTrack, "/next/<int:user>")
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(host="0.0.0.0")
--- recsys/sim/sim/run.py
+++ recsys/sim/sim/run.py
@@ -1,6 +1,9 @@
 import argparse
+import cmd
 from dataclasses import dataclass, asdict
+import numpy as np
 import pandas as pd
+import scipy.stats as ss
 import tqdm
 
 import yaml
@@ -12,17 +15,18 @@ from sim.envs.config import RecEnvConfigSchema, RecEnvConfig
 
 @dataclass
 class EpisodeStats:
+    day: int
     episode: int
     reward: float = 0.0
     steps: int = 0
 
 
-def run_episode(episode: int, env: RecEnv, recommender: Recommender):
+def run_episode(day: int, episode: int, env: RecEnv, recommender: Recommender):
     previous_observation = env.reset()
     done = False
     reward = 0
 
-    stats = EpisodeStats(episode)
+    stats = EpisodeStats(day, episode)
 
     while not done:
         action = recommender.recommend(previous_observation, reward, done)
@@ -34,7 +38,9 @@ def run_episode(episode: int, env: RecEnv, recommender: Recommender):
     return stats
 
 
-def run_experiment(env: RecEnv, episodes: int, dummy: bool, config: RecEnvConfig):
+def run_experiment(
+    day: int, env: RecEnv, episodes: int, dummy: bool, config: RecEnvConfig
+):
     if dummy:
         recommender = DummyRecommender(env.action_space)
     else:
@@ -42,7 +48,7 @@ def run_experiment(env: RecEnv, episodes: int, dummy: bool, config: RecEnvConfig
 
     stats = []
     for episode_id in tqdm.trange(episodes):
-        stats.append(run_episode(episode_id, env, recommender))
+        stats.append(run_episode(day, episode_id, env, recommender))
     return stats
 
 
@@ -70,13 +76,48 @@ def main():
 
     config = RecEnvConfigSchema().load(yaml.full_load(open(args.config)))
 
+    stats = []
     with RecEnv(config) as env:
         env.seed(args.seed)
-        stats = run_experiment(env, args.episodes, args.dummy_recommender, config)
 
-    print(
-        f"## Experiment summary\n\n{pd.DataFrame([asdict(s) for s in stats]).describe().to_markdown()}"
+        day = 1
+        while True:
+            stats.extend(
+                run_experiment(day, env, args.episodes, args.dummy_recommender, config)
+            )
+
+            time_control = TimeControl()
+            time_control.cmdloop(
+                f"End of day {day}. Would you like to start a new day?"
+            )
+            if time_control.done:
+                break
+            else:
+                day += 1
+
+    result = (
+        pd.DataFrame([asdict(s) for s in stats])
+        .groupby("day")[["reward", "steps"]]
+        .agg([np.mean, ss.sem])
     )
+    print(f"## Experiment results summary\n\n{result.to_markdown()}")
+
+
+class TimeControl(cmd.Cmd):
+    prompt = "(y/n) "
+
+    def __init__(self):
+        super().__init__()
+        self.done = False
+
+    def do_y(self, arg):
+        print("Moving to the next day!")
+        return True
+
+    def do_n(self, arg):
+        print("Ending the simulation")
+        self.done = True
+        return True
 
 
 if __name__ == "__main__":
