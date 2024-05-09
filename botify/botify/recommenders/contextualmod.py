1d8cf1c60f16591c7c2a9c455941c8ea2bd57768
CoffeBank <ed.yarulin@mail.ru>
2024-05-09 14:46:56 +0300

    update contextual mod

--- botify/botify/recommenders/contextualmod.py
+++ botify/botify/recommenders/contextualmod.py
@@ -13,10 +13,6 @@ class ContextualMod(Recommender):
 
     def recommend_next(self, user: int, prev_track: int, prev_track_time: float) -> int:
 
-        previous_track = self.tracks_redis.get(prev_track)
-        if previous_track is None:
-            return self.fallback.recommend_next(user, prev_track, prev_track_time)
-
         if prev_track_time < TRACK_DURATION:
             recommendations = self.recommendations_redis.get(user)
             if recommendations is not None:
@@ -26,9 +22,16 @@ class ContextualMod(Recommender):
             else:
                 return self.fallback.recommend_next(user, prev_track, prev_track_time)
 
+        previous_track = self.tracks_redis.get(prev_track)
         recommendations = self.catalog.from_bytes(previous_track)
         if not recommendations:
-            return self.fallback.recommend_next(user, prev_track, prev_track_time)
+            recommendations = self.recommendations_redis.get(user)
+            if recommendations is not None:
+                shuffled = list(self.catalog.from_bytes(recommendations))
+                random.shuffle(shuffled)
+                return shuffled[0]
+            else:
+                return self.fallback.recommend_next(user, prev_track, prev_track_time)
 
         shuffled = list(recommendations)
         random.shuffle(shuffled)
